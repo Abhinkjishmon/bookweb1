63b3eaab8817c0c86eedfe9fafd01802e66054eb
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-01-26 21:31:38 +0530

    13th commit

--- script.js
+++ script.js
@@ -33,6 +33,14 @@ window.onload =() =>{
     else{
         document.querySelector('.header .header-2').classList.remove('active');
     }
+    fadeOut();
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active')
+}
+function fadeOut(){
+  setTimeout(loader, 4000);
 }
 
 
@@ -102,4 +110,62 @@ var swiper = new Swiper(".arrivals-slider", {
         slidesPerView: 3,
       },
     },
+});
+
+
+
+
+
+
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
